20336891a1c0f9447142c45c9bc9d8d13103830c
Алексей Бороденко <phoenixx8483@gmail.com>
2024-09-23 07:03:21 +0300

    Исправление отображения точки в поинтах, не знаю, как она там очутилась, в css написал list-style-type: none;

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -1,7 +1,7 @@
 import { getRandomPoint } from '../mock/points.js';
 import { mockOffers } from '../mock/offers.js';
 import { mockDestinations } from '../mock/destination.js';
-const POINT_COUNT = 1; //Количество поинтов (сколько раз отрисуется поинт getRandomPoint)
+const POINT_COUNT = 4; //Количество поинтов (сколько раз отрисуется поинт getRandomPoint)
 
 export default class PointsModel {
   points = Array.from({length: POINT_COUNT}, getRandomPoint); //Записываем в points массив поинтов
--- src/view/trip-point-view.js
+++ src/view/trip-point-view.js
@@ -8,7 +8,7 @@ const createListTemplate = (point, offers, destination) => {
   const createEventOfferTemplate = (title, price) => (`
     <li class="event__offer">
       <span class="event__offer-title">${title}</span>
-        &plus;&euro;&nbsp;
+
       <span class="event__offer-price">${price}</span>
                   </li>
   `);
