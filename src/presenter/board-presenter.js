67ac5e1f2b78bfe3bd3b5d8f4f9dac6659169eeb
Алексей Бороденко <phoenixx8483@gmail.com>
2024-10-06 03:56:07 +0300

    Перенес все остальные эл-ты в презентер, сделал приватными методы и свойства

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,16 +1,22 @@
 import TripInfoView from '../view/trip-info-view.js'; //Инфо в шапке про маршрут
+import SortView from '../view/trip-event-view.js'; //Сортировка DAY, EVENT, PRICE
 import PointView from '../view/trip-point-view.js'; //Точка маршрута
 import OffersView from '../view/trip-edit-point-view.js';//Форма редактирования
-import { render, replace } from '../framework/render.js';
 import EmptyListView from '../view/list-message-view.js';//Пустой лист без поинтов
+
+import { render, replace } from '../framework/render.js';
 import { EMPTY_LIST } from '../constant/const.js';
+
 const siteMainElement = document.querySelector('.page-body');
 const siteTripInfo = siteMainElement.querySelector('.trip-info'); // Инфо в шапке про маршрут
+const siteEventsElement = siteMainElement.querySelector('.trip-events');
 export default class BoardPresenter {
   #container = null;
   #pointsModel = null;
   #boardPoints = [];
-  #emptyList = new EmptyListView({message: EMPTY_LIST.EVERYTHING});
+  #emptyList = new EmptyListView({message: EMPTY_LIST.EVERYTHING}); //Нет поинтов
+  #sortView = new SortView(); //Приватное св-во Сортировки
+  #infoView = new TripInfoView(); //Информация в шапке
 
   constructor({container, pointsModel}) {
     this.#container = container;
@@ -69,7 +75,8 @@ export default class BoardPresenter {
   }
 
   #renderBoard() { //Отображение всех остальных компонентов
-    render(new TripInfoView(), siteTripInfo); // Отображение информации в шапке про маршрут
+    render(this.#infoView, siteTripInfo); // Отображение информации в шапке про маршрут
+    render(this.#sortView, siteEventsElement); //Сортировка Day, Price...
     this.boardOffers = [...this.#pointsModel.offers];
     for (let i = 0; i < this.#boardPoints.length; i++) {
       this.#renderPoints(this.#boardPoints[i]);
